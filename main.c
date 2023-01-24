d76642b4aa33d110bc70bc3cb23da14841137b86
ToyotaPrius <114371673+ToyotaPrius9@users.noreply.github.com>
2023-01-24 17:58:58 +0700

    Update main.c

--- main.c
+++ main.c
@@ -39,21 +39,21 @@ void compute_outputs(int A, int B, int C, int D, int *y1, int *y2) {
 }
 
 int main() {
-  for (int A = 0; A <= 1; A++) {
-    for (int B = 0; B <= 1; B++) {
-      for (int C = 0; C <= 1; C++) {
-        for (int D = 0; D <= 1; D++) {
-          int y1, y2;
-          printf("\033c");
-          compute_outputs(A, B, C, D, &y1, &y2);
-          printf("\nCurrent State = %d%d | Input = %d%d \n\nOutput = %d%d\n\n", A, B, C, D, y1, y2);
-          sleep(1.8); 
-          
-          
-
-        }
-      }
-    }
-  }
+  printf("State Index:\nPark = 00\nReverse = 01\nNeutral = 10\nDrive = 11\n\n[Input Them Without Space, For Example:\n0011 -> park state to drive state\n\n");
+  int input;
+  int A, B, C, D;
+  int y1, y2;
+  printf("Enter value for Current State and Input State: ");
+  scanf("%4d", &input);
+  A = input / 1000;
+  B = (input / 100) % 10;
+  C = (input / 10) % 10;
+  D = input % 10;
+  printf("\033c");
+  compute_outputs(A, B, C, D, &y1, &y2);
+  printf("\nCurrent State = %d%d | Input = %d%d \n\nOutput = %d%d\n\n", A, B, C, D, y1, y2);
   return 0;
 }
+
+// gcc -o main main.c
+// ./main
